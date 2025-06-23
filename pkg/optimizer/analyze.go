e8ee4097d9389d31228cdf4beb4834ff8033f047
zhuhuijun <zhuhuijunzhj@gmail.com>
2025-06-23 14:27:14 +0800

    refactor: simplify instruction analysis tests and enhance CSV data loading functionality

--- pkg/optimizer/analyze.go
+++ pkg/optimizer/analyze.go
@@ -1,6 +1,10 @@
 package optimizer
 
-import "github.com/beepfd/bpf-optimizer/pkg/bpf"
+import (
+	"math"
+
+	"github.com/beepfd/bpf-optimizer/pkg/bpf"
+)
 
 func (a *InstructionAnalysis) ALU(opcode uint8, dst int, src int) {
 	msb := opcode & 0xF0
@@ -25,11 +29,14 @@ func (a *InstructionAnalysis) ALU(opcode uint8, dst int, src int) {
 
 func (a *InstructionAnalysis) JMP(opcode uint8, dst int, src int, off int16, imm int32) {
 	msb := opcode & 0xF0
+	if opcode == 5 {
+		a.UsedReg = []int{}
+		a.Offset = off
+		return
+	}
+
 	switch msb {
 	case bpf.JMP_CALL:
-		a.IsCall = true
-		a.UpdatedReg = 0
-
 		// Handle different BPF helper functions
 		switch imm {
 		case 12: // tail call
@@ -37,32 +44,44 @@ func (a *InstructionAnalysis) JMP(opcode uint8, dst int, src int, off int16, imm
 			a.UsedStack = []int16{0, 0}
 		case 1, 3, 23, 44: // map lookup, delete
 			a.UsedReg = []int{1, 2}
+			a.UpdatedReg = 0
 		case 2, 69: // map update
 			a.UsedReg = []int{1, 2, 3, 4}
+			a.UpdatedReg = 0
 		case 4, 51: // map lookup
 			a.UsedReg = []int{1, 2, 3}
+			a.UpdatedReg = 0
 		case 5, 7, 8: // various helpers
 			// only updates r0
+			a.UpdatedReg = 0
 		case 9, 10, 11: // complex helpers
 			a.UsedReg = []int{1, 2, 3, 4, 5}
+			a.UpdatedReg = 0
 		default:
 			a.UsedReg = []int{1, 2, 3, 4, 5}
+			a.UpdatedReg = 0
+			a.IsCall = true
 		}
 	case bpf.JMP_EXIT:
 		a.UsedReg = []int{0}
 		a.IsExit = true
-	case 0x05: // unconditional jump
-		a.Offset = off
 	default: // conditional jump
 		a.UsedReg = []int{dst, src}
 		a.Offset = off
 	}
 }
 
+// 计算BPF指令的size（以位为单位）
+func calculateSizeBits(opcode uint8) int {
+	sizeField := opcode & 0x18
+	exponent := float64(sizeField)/8.0 + 3.0
+	return int(math.Pow(2, exponent))
+}
+
 func (a *InstructionAnalysis) STX(opcode uint8, dst int, src int, off int16, imm int32) {
 	msb := opcode & 0xE0
 	if msb == bpf.BPF_MEM || msb == bpf.BPF_MEMSX || msb == bpf.BPF_ATOMIC {
-		size := int16(1 << ((opcode & 0x18) >> 3))
+		size := int16(calculateSizeBits(opcode))
 		if dst == 10 { // stack pointer
 			a.UpdatedStack = []int16{off, size}
 			a.UsedReg = []int{src}
@@ -75,7 +94,7 @@ func (a *InstructionAnalysis) STX(opcode uint8, dst int, src int, off int16, imm
 func (a *InstructionAnalysis) ST(opcode uint8, dst int, src int, off int16, imm int32) {
 	msb := opcode & 0xE0
 	if msb == bpf.BPF_MEM || msb == bpf.BPF_MEMSX || msb == bpf.BPF_ATOMIC {
-		size := int16(1 << ((opcode & 0x18) >> 3))
+		size := int16(calculateSizeBits(opcode))
 		if dst == 10 { // stack pointer
 			a.UpdatedStack = []int16{off, size}
 		}
@@ -85,7 +104,7 @@ func (a *InstructionAnalysis) ST(opcode uint8, dst int, src int, off int16, imm 
 func (a *InstructionAnalysis) LDX(opcode uint8, dst int, src int, off int16, imm int32) {
 	msb := opcode & 0xE0
 	if msb == bpf.BPF_MEM || msb == bpf.BPF_MEMSX {
-		size := int16(1 << ((opcode & 0x18) >> 3))
+		size := int16(calculateSizeBits(opcode))
 		a.UpdatedReg = dst
 		if src == 10 { // stack pointer
 			a.UsedStack = []int16{off, size}
